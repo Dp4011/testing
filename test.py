d29dda6b7e45bbc3fef2f26e4efd442b314e2aa3
Gummidi Durga Prasad <gummidi.10739134@ltimindtree.com>
2025-02-19 11:27:43 +0530

    second commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+
+username = 'my_username'
+
+token = 'token'
+
+# GitHub API URL to list repositories
+url = f'https://api.github.com/users/{username}/repos'
+
+# Make a request to the GitHub API
+response = requests.get(url, auth=(username, token))
+
+# Check if the request was successful
+if response.status_code == 200:
+    repos = response.json()
+    print("List of repositories:")
+    for repo in repos:
+        print(repo['name'])
+else:
+    print(f"Failed to retrieve repositories: {response.status_code}")
